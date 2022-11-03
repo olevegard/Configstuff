64b99b3805b4e237476cbbdf865bb421f070a6e1
Ole Vegard Mythe Moland <oleveg@gmail.com>
2022-11-03 14:30:18 +0100

    init.lua : Added autosave

--- init.lua
+++ init.lua
@@ -42,6 +42,7 @@ require('packer').startup(function(use)
   use 'L3MON4D3/LuaSnip'              -- Snippets plugin
   use 'rafi/awesome-vim-colorschemes' -- Colorschemes ( including jellybeans )
   use 'preservim/nerdtree'            -- Tree view
+  use 'Pocco81/auto-save.nvim'       -- Autosave
 
   use 'tpope/vim-surround' -- Change surronding brackets/quotes/tabs++
   -- use 'tpope/fugitive' -- Git support
@@ -53,6 +54,7 @@ require('packer').startup(function(use)
   }
 
   use 'nvim-treesitter/nvim-treesitter-context'
+
 
   if packer_bootstrap then
     require('packer').sync()
@@ -246,6 +248,44 @@ require'treesitter-context'.setup{
     separator = '-',
 }
 
+
+-- Autosave
+-- =====================================================================================================================================
+require'auto-save'.setup{
+    enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+    execution_message = {
+		message = function() -- message to print on save
+			return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+		end,
+		dim = 0.18, -- dim the color of `message`
+		cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+	},
+    trigger_events = {"InsertLeave", "TextChanged"}, -- vim events that trigger auto-save. See :h events
+	-- function that determines whether to save the current buffer or not
+	-- return true: if buffer is ok to be saved
+	-- return false: if it's not ok to be saved
+	condition = function(buf)
+		local fn = vim.fn
+		local utils = require("auto-save.utils.data")
+
+		if
+			fn.getbufvar(buf, "&modifiable") == 1 and
+			utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+			return true -- met condition(s), can save
+		end
+		return false -- can't save
+	end,
+    write_all_buffers = false, -- write all buffers when the current one meets `condition`
+    debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
+	callbacks = { -- functions to be executed at different intervals
+		enabling = nil, -- ran when enabling auto-save
+		disabling = nil, -- ran when disabling auto-save
+		before_asserting_save = nil, -- ran before checking `condition`
+		before_saving = nil, -- ran before doing the actual save
+		after_saving = nil -- ran after doing the actual save
+	}
+}
+
 -- Language servers
 -- =====================================================================================================================================
 require('lspconfig')['pyright'].setup{
@@ -322,7 +362,7 @@ vim.keymap.set('n', '<Leader>gbd', ':! go build %<CR>')
 vim.keymap.set('n', '<Leader>gor', 'go run %<CR>')
 
 -- Dart + Flutter
-vim.keymap.set('n', '<Leader>', ':! flutter format --line-length 120 % <CR>')
+vim.keymap.set('n', '<Leader>dfmt', ':! flutter format --line-length 120 % <CR>')
 vim.keymap.set('n', '<Leader>dtest', ':! flutter test % <CR>')
 vim.keymap.set('n', '<Leader>pget ', ':! flutter pub get <CR>')
 vim.keymap.set('n', '<Leader>prun', ':! flutter pub run build_runner build --delete-conflicting-outputs <CR>')
@@ -381,7 +421,7 @@ vim.opt.wrap = false  -- Disable implicit line breaks
 -- =====================================================================================================================================
 -- File system
 -- =====================================================================================================================================
-vim.opt.backup = true     -- Keep a backup file
+vim.opt.backup = false     -- Keep a backup file
 vim.opt.swapfile = false  -- No swap file
 vim.opt.autoread = true   -- Automatically reload
 
